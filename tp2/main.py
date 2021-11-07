5b1ee1720f940feae6d7cd33005a17dcf89884e3
nassimb0u <in_boulechfar@esi.dz>
2021-11-07 21:03:06 +0100

    Refactor Pij so it looks like the given def

--- tp2/main.py
+++ tp2/main.py
@@ -1,4 +1,4 @@
-W = 4
+W = 5
 N = 5
 Wi = list(range(N+1))
 Vi = list(range(N+1))
@@ -6,23 +6,19 @@ Vi = list(range(N+1))
 dp = []
 
 def P(i, j):
+    if i==0 or j==0:
+        return 0, [0]*i
     if j < Wi[i]:
         return dp[i-1][j][0], dp[i-1][j][1]+[0]
+    if dp[i-1][j][0] > dp[i-1][j-Wi[i]][0]+Vi[i]:
+        return dp[i-1][j][0], dp[i-1][j][1]+[0]
     else:
-        if dp[i-1][j][0] > dp[i-1][j-Wi[i]][0]+Vi[i]:
-            return dp[i-1][j][0], dp[i-1][j][1]+[0]
-        elif dp[i-1][j][0] < dp[i-1][j-Wi[i]][0]+Vi[i]:
-            return dp[i-1][j-Wi[i]][0]+Vi[i], dp[i-1][j-Wi[i]][1]+[1]
-        else:
-            return dp[i-1][j][0], dp[i-1][j][1]+[0], dp[i-1][j-Wi[i]][1]+[1]
+        return dp[i-1][j-Wi[i]][0]+Vi[i], dp[i-1][j-Wi[i]][1]+[1]
 
 for i in range(N+1):
     dp.append([])
     for j in range(W+1):
-        if i==0 or j==0:
-            dp[i].append((0, [0]*i))
-        else:
-            dp[i].append(P(i, j))
+        dp[i].append(P(i, j))
 
 print(dp[N][W])
     
