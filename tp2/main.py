16164779d851fb5a6dbe1674e90519dd3e9e5e38
nassimb0u <in_boulechfar@esi.dz>
2021-11-07 21:54:17 +0100

    Create solver class Knapsack

--- tp2/main.py
+++ tp2/main.py
@@ -1,24 +1,9 @@
-W = 5
+from knapsack import Knapsack
+
+W = 4
 N = 5
 Wi = list(range(N+1))
 Vi = list(range(N+1))
 
-dp = []
-
-def P(i, j):
-    if i==0 or j==0:
-        return 0, [0]*i
-    if j < Wi[i]:
-        return dp[i-1][j][0], dp[i-1][j][1]+[0]
-    if dp[i-1][j][0] > dp[i-1][j-Wi[i]][0]+Vi[i]:
-        return dp[i-1][j][0], dp[i-1][j][1]+[0]
-    else:
-        return dp[i-1][j-Wi[i]][0]+Vi[i], dp[i-1][j-Wi[i]][1]+[1]
-
-for i in range(N+1):
-    dp.append([])
-    for j in range(W+1):
-        dp[i].append(P(i, j))
-
-print(dp[N][W])
-    
+k = Knapsack(W, N, Wi, Vi)
+print(k.get_solution())
