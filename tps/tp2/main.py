c71ff61f08be9c480f328c28ce1609d0e49e0f18
nassimb0u <in_boulechfar@esi.dz>
2021-11-08 11:23:27 +0100

    Organize repo structure

---
+++ tps/tp2/main.py
@@ -0,0 +1,12 @@
+from knapsack import Knapsack
+
+W = int(input("Enter the capacity W="))
+N = int(input("Enter the number of items N="))
+Wi = list(map(int, input(f"Enter {N} comma separated values for items weights:\n").split(',')))
+Vi = list(map(int, input(f"Enter {N} comma separated values for items values:\n").split(',')))
+try:
+    assert len(Wi)==N and len(Vi)==N
+    k = Knapsack(W, N, Wi, Vi, all=True)
+    print("The solution is ", k.get_solution())
+except:
+    print("Wrong number of weights or values")
