bcdd7f5c17ba78c60e5e0c53988cb54d4b7f4535
nassimb0u <in_boulechfar@esi.dz>
2021-11-11 15:15:36 +0100

    Change interactive input format
    Use space separated (or any whitespace) instead of comma

--- tps/tp2/main.py
+++ tps/tp2/main.py
@@ -1,14 +1,14 @@
 from knapsack import Knapsack
 
-#W = int(input("Enter the capacity W="))
-#N = int(input("Enter the number of items N="))
-#Wi = list(map(int, input(f"Enter {N} comma separated values for items weights:\n").split(',')))
-#Vi = list(map(int, input(f"Enter {N} comma separated values for items values:\n").split(',')))
+W = int(input("Enter the capacity W="))
+N = int(input("Enter the number of items N="))
+Wi = list(map(int, input(f"Enter {N} space separated values for items weights:\n").split()))
+Vi = list(map(int, input(f"Enter {N} space separated values for items values:\n").split()))
 
-W = 5
-N = 5
-Wi = list(range(1, N+1))
-Vi = list(range(1, N+1))
+#W = 5
+#N = 5
+#Wi = list(range(1, N+1))
+#Vi = list(range(1, N+1))
 
 try:
     assert len(Wi)==N and len(Vi)==N
