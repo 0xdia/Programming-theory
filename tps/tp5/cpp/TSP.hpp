7983213f8422efcb2c6170661037c625a3d329a2
Dhai Eddine Zebbiche <joseph.zebbiche@gmail.com>
2021-12-10 12:04:16 +0100

    add tsp namespace, city class

---
+++ tps/tp5/cpp/TSP.hpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+namespace tsp {
+  class city {
+    public:
+      city() {}
+      city(string name): _name(name) {}
+
+    private:
+      string _name;
+  }
+};
