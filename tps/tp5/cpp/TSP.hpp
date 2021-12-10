d636eb782253c2a3b41d9a20dd2e2607e7fe5438
Dhai Eddine Zebbiche <joseph.zebbiche@gmail.com>
2021-12-10 12:07:36 +0100

    add road class

--- tps/tp5/cpp/TSP.hpp
+++ tps/tp5/cpp/TSP.hpp
@@ -7,9 +7,21 @@ namespace tsp {
   class city {
     public:
       city() {}
-      city(string name): _name(name) {}
+      city(string name, int id): _name(name), _id(id) {}
 
     private:
       string _name;
+      int    _id;
+  };
+
+  class road {
+    public:
+      road() {}
+      road(int city1, int city2, int distance) 
+        : _city1(city1), _city2(city2), _distance(distance)
+        {}
+
+    private:
+      int _city1, int _city2, int _distance;
   }
 };
